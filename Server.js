83a6b258eac17d0400b2235e9d0c7dcf6a527f12
Pradeep <pradeepsingh17101998@gmail.com>
2021-01-08 00:35:51 +0530

    update again

--- Server.js
+++ Server.js
@@ -1,30 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 // MongoDB Connectivity
-const connectDB = require('./Config/db');
-
+const connectDB = require("./Config/db");
 
 connectDB();
 
-app.use(express.json());
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 // All Routes
-app.use('/api/user', require('./routes/User'));
-app.use('/api/auth',require('./routes/Auth'));
-app.use('/api/articles',require('./routes/Article'));
-app.use('/api/tags',require('./routes/Tag'));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/articles", require("./routes/articles"));
+app.use("/api/tags", require("./routes/tags"));
 
 // Server Static assests in production
-if(process.env.NODE_ENV === 'production'){
-
-    app.use(express.static('client/build'));
-    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
 }
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000; 
-
-app.listen(PORT , () => {
-    console.log(`Server Started at port - ${PORT}`);
-})
+app.listen(PORT, () => console.log(`Server Started at port - ${PORT}`));
