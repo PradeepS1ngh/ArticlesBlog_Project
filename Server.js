f026c8ae58933f14968a954c73acc80da9d70c8f
Pradeep <pradeepsingh17101998@gmail.com>
2021-01-07 23:37:40 +0530

    Prepare for Deploy

--- Server.js
+++ Server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 // MongoDB Connectivity
 const connectDB = require('./Config/db');
+
+
 connectDB();
 
 app.use(express.json());
@@ -12,6 +14,14 @@ app.use('/api/user', require('./routes/User'));
 app.use('/api/auth',require('./routes/Auth'));
 app.use('/api/articles',require('./routes/Article'));
 app.use('/api/tags',require('./routes/Tag'));
+
+// Server Static assests in production
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
+
 
 const PORT = process.env.PORT || 5000; 
 
